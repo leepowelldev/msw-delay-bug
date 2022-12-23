c7d88fab0bca7175d1fdf106b93d8950f06bc0e6
Lee Powell <powell.lee@gmail.com>
2022-12-23 10:16:32 +0000

    Added additional tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,17 +4,7 @@ import { screen, render, waitFor } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-const server = setupServer(
-  rest.get('https://mydomain.com', (_, res, ctx) => {
-    return res(
-      ctx.delay(), // This passes
-      // ctx.delay(1000), // This fails
-      ctx.json({
-        message: 'success',
-      })
-    );
-  })
-);
+const server = setupServer();
 
 beforeAll(() => {
   server.listen();
@@ -28,7 +18,63 @@ beforeEach(() => {
   server.resetHandlers();
 });
 
-test('data is fetched', async () => {
+test('data is fetched with no delay', async () => {
+  server.use(
+    rest.get('https://mydomain.com', (_, res, ctx) => {
+      return res(
+        ctx.json({
+          message: 'success',
+        })
+      );
+    })
+  );
+
+  render(<App />);
+
+  await waitFor(
+    () => {
+      expect(screen.getByText('success')).toBeTruthy();
+    },
+    // Give a long timeout just to be sure
+    { timeout: 10000 }
+  );
+});
+
+test('data is fetched with short delay', async () => {
+  server.use(
+    rest.get('https://mydomain.com', (_, res, ctx) => {
+      return res(
+        ctx.delay(),
+        ctx.json({
+          message: 'success',
+        })
+      );
+    })
+  );
+
+  render(<App />);
+
+  await waitFor(
+    () => {
+      expect(screen.getByText('success')).toBeTruthy();
+    },
+    // Give a long timeout just to be sure
+    { timeout: 10000 }
+  );
+});
+
+test('data is fetched with long delay', async () => {
+  server.use(
+    rest.get('https://mydomain.com', (_, res, ctx) => {
+      return res(
+        ctx.delay(1000),
+        ctx.json({
+          message: 'success',
+        })
+      );
+    })
+  );
+
   render(<App />);
 
   await waitFor(
